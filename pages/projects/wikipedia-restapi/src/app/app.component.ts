8f5bd207898e4f32dc634a4fa25f0dc723d983db
lufecrx <luizfelipecrx@gmail.com>
2024-01-23 14:02:52 -0300

    Fazer mudanças e correções necessárias

--- pages/projects/wikipedia-restapi/src/app/app.component.ts
+++ pages/projects/wikipedia-restapi/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiapiService } from './services/wikiapi.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wikipedia-restapi';
+
+  articles: any[] = [];
+  selectedArticle: any = '';
+
+  constructor(private wikiService: WikiapiService) {}
+
+  search(term: string) {
+    this.wikiService.search(term).subscribe((response: any) => {
+      this.wikiService.getArticlesDetails(response.query.search).subscribe((details: any[]) => {
+        this.articles = response.query.search.map((result: any, index: number) => ({
+          title: result.title,
+          snippet: result.snippet,
+          fullurl: details[index].fullurl,
+        }));
+        this.selectedArticle = null;
+      });
+    });
+  }
 }
--- pages/projects/wikipedia-restapi/src/app/app.module.ts
+++ pages/projects/wikipedia-restapi/src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { FormsModule } from '@angular/forms';
+import { ArticleComponent } from './components/article/article.component';
+import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HttpClient, HttpClientJsonpModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
